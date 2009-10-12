24b01e4f28dc93625621d5f2d90c487be7a88f17
ike-dai <dai.ikd123@gmail.com>
2009-10-13 07:01:54 +0900

    modify

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,31 @@
 class TopController < ApplicationController
+  def list
+    render :action => "list"
+  end
+
+  def new
+  end
+
+  def add    
+    @members = params[:members].first
+    p params[:candidates1]
+    p params[:candidates2]
+    p params[:candidates3]
+    p params[:candidates4]
+
+    @days = params[:candidates1]
+  end
+
+#Ajax処理
+  def add_candidate
+    p params[:count]
+    name = 'candidates' + params[:count]   
+    render :inline => "<br>" + "<%= datetime_select('#{name}','time[]',{ :start_year => Time.now.year, :end_year => Time.now.year+2, :use_month_numbers => true, :minute_steps => 15} ) %>"
+  end
+
+  def increment_count i
+    @i = i.to_i + 1
+  end
   def input
     render :action => "input"
   end
--- tmp/war/WEB-INF/app/controllers/top_controller.rb
+++ tmp/war/WEB-INF/app/controllers/top_controller.rb
@@ -1,4 +1,31 @@
 class TopController < ApplicationController
+  def list
+    render :action => "list"
+  end
+
+  def new
+  end
+
+  def add    
+    @members = params[:members].first
+    p params[:candidates1]
+    p params[:candidates2]
+    p params[:candidates3]
+    p params[:candidates4]
+
+    @days = params[:candidates1]
+  end
+
+#Ajax処理
+  def add_candidate
+    p params[:count]
+    name = 'candidates' + params[:count]   
+    render :inline => "<br>" + "<%= datetime_select('#{name}','time[]',{ :start_year => Time.now.year, :end_year => Time.now.year+2, :use_month_numbers => true, :minute_steps => 15} ) %>"
+  end
+
+  def increment_count i
+    @i = i.to_i + 1
+  end
   def input
     render :action => "input"
   end
@@ -8,9 +35,6 @@ class TopController < ApplicationController
     @voter_name = params[:data][:name]
     @result = params[:data][:category]
     Voters.create(:name => @voter_name, :result => @result ).save!
-    voter_list = Voters.all
-    p voter_list.first.name
-    p voter_list.first.result
     render :action => "result"
   end
 end
